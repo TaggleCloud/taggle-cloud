3fd40dcfa882fd7d7785dfd0005b436f809710d5
Jonathan <jbender@jbender-ubuntu.(none)>
2012-09-11 17:24:12 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TaggleCloud::Application.config.secret_token = '957e4f16a597aeae29bac632dcebbe688dd40bc0122ccb2e7d2ca8a5f3c59c354d4cd19a949e6746386171eb34fd6f77c22a15fc74b9076421dec0ed1032abd8'
