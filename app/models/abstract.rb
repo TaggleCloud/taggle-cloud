ea109ed4764a8bf7ec37ea8ab679ead50789c013
Jon <jetan@andrew.cmu.edu>
2012-09-27 00:00:23 -0400

    auto-generate tags of all words in body

--- app/models/abstract.rb
+++ app/models/abstract.rb
@@ -1,6 +1,7 @@
 require 'levenshtein'
 
 class Abstract < ActiveRecord::Base
+  after_create :create_tags
   attr_accessible :body, :user_id
 
   belongs_to :user
@@ -10,5 +11,12 @@ class Abstract < ActiveRecord::Base
   #http://en.wikibooks.org/wiki/Algorithm_Implementation/Strings/Levenshtein_distance#Ruby
   def levenshtein(b)
     Levenshtein.distance(self.body, b.body)
+  end
+  
+  tags = :body.split(' ')
+  
+  def create_tags
+    tags.each do |tag|
+      Tag.create :value => tag
   end
 end
