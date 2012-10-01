be933ea01db06769052d8d38a2cf55a2ce8b1667
Jon <jetan@andrew.cmu.edu>
2012-10-01 15:34:01 -0400

    file upload backend

--- app/models/conference.rb
+++ app/models/conference.rb
@@ -3,4 +3,11 @@ class Conference < ActiveRecord::Base
 
   has_many :attendances
   has_many :abstracts
+  
+  def upload
+    uploaded_io = params[:conference][:csv]
+    File.open(Rails.root.join('public', 'uploads', uploaded_io.original_filename), 'w') do |file|
+      file.write(uploaded_io.read)
+    end
+  end
 end
