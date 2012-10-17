aadf1f2a2cbc953c6d42996b38ef3bdb436abda5
Jonathan Bender <jlbender@gmail.com>
2012-10-17 16:45:08 -0400

    lots of changes that don't all work

--- app/models/conference.rb
+++ app/models/conference.rb
@@ -8,22 +8,26 @@ class Conference < ActiveRecord::Base
   has_many :connections, :through => :attendances
   
   def upload(uploaded_io)
+
+    attendances, abstracts = [], []
+
     CSV.parse(uploaded_io.read) do |row|
       if row[5] && valid_email(row[5])
-        at = Attendance.new(:registered_email => row[5], :conference_id => self.id, :first_name => row[3], :last_name => row[4], :organization => row[2])
-        e = Email.where(:mail_address => row[5]).first
-        if e
-          at.user_id = e.user_id
-        end
-        at.save
+        attendances << Attendance.new(:registered_email => row[5], :conference_id => self.id, :first_name => row[3], :last_name => row[4], :organization => row[2])
+        # e = Email.where(:mail_address => row[5]).first
+        # if e
+        #   at.user_id = e.user_id
+        # end
         abstract = sanitize_string(row[7]) if row[7]
         if abstract
-          ab = Abstract.new(:body => abstract, :attendance_id => at.id, :user_id => at.user_id)
-          ab.save
+          abstracts << Abstract.new(:body => abstract, :attendance_id => at.id, :user_id => at.user_id)
         end
       end
     end
-    Connection.csv_upload_builder(self)
+    # Connection.csv_upload_builder(self)
+
+    Attendance.import(attendances)
+    Abstract.import(abstracts)
   end
 
   def valid_email(email)
