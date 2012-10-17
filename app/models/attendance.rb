7b24ed75962b0c9c8c65e2b2e164fad55731b83d
Jonathan Bender <jlbender@gmail.com>
2012-10-17 13:57:48 -0400

    add name and org to attendance

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,5 +1,5 @@
 class Attendance < ActiveRecord::Base
-  attr_accessible :conference_id, :registered_email, :user_id
+  attr_accessible :conference_id, :registered_email, :user_id, :first_name, :last_name, :organization
 
   belongs_to :conference
   belongs_to :user
