6d0028710a9c5334d11d6fe98fcec4efc6177ed0
Jonathan Bender <jlbender@gmail.com>
2012-10-23 15:50:56 -0400

    got the hi there

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,11 @@
 TaggleCloud::Application.routes.draw do
 
-  resources :user_interests
-
-  resources :interests
-
   resources :conferences do
     match "connections" => "conferences#connections"
     resources :attendances, :as => :attendees, :path => 'attendees', :on => :member
   end
   
   resources :users do
-    match "/users/:id" => "users#profile"
     resources :attendances
   end
 
@@ -20,6 +15,7 @@ TaggleCloud::Application.routes.draw do
 
   scope :constraints => lambda{ |req| !req.session[:user_id].blank? } do
     root :to => "users#dashboard", :as => :dashboard
+    match "profile" => "users#profile", :as => :profile
   end
 
   match "/credits" => "landings#credits"
