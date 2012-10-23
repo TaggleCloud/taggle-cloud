6973eed7b2870206f753a1b995b362ad9cc44f36
tony <sarsntony@gmail.com>
2012-10-23 18:02:09 -0400

    Create basic user profile

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
-  provider :linkedin, "gaywpkqsa345", "3FpYnz57WmsJsTVA"
-  provider :google_oauth2, "179239928840.apps.googleusercontent.com", "lzcxOJO4N5LPcuTbo_pJ-Gbm", {access_type: 'online', approval_prompt: ''}
+  provider :linkedin, "gaywpkqsa345", "3FpYnz57WmsJsTVA", :scope => 'r_fullprofile r_emailaddress r_network'
+  provider :google_oauth2, "179239928840.apps.googleusercontent.com", "lzcxOJO4N5LPcuTbo_pJ-Gbm", {:scope => 'userinfo.profile,userinfo.email,plus.me'}
   provider :facebook, "268051749965205", "71bb1eb11aac5b8c368a2f2e42a46bc0"
 
 end
