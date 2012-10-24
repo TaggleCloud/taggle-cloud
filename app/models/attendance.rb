6a70513fd90acfb5603c171e1cd5025873f5fc91
Jonathan Bender <jlbender@gmail.com>
2012-10-24 14:02:38 -0400

    more sidekiq cleanup

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,6 +1,8 @@
 class Attendance < ActiveRecord::Base
   attr_accessible :conference_id, :registered_email, :user_id, :first_name, :last_name, :organization
 
+  after_create :make_connections
+
   belongs_to :conference
   belongs_to :user
   belongs_to :connection
@@ -9,6 +11,10 @@ class Attendance < ActiveRecord::Base
 
   def name
     return first_name.to_s + " " + last_name.to_s
+  end
+
+  def make_connections
+    ConnectionBuilder.perform_async(:conference_id, :id)
   end
 
 end
