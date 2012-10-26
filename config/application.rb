99c23bed3600cec468cacc61cd8307ca1c16859b
Jonathan Bender <jlbender@gmail.com>
2012-10-26 01:35:18 -0400

    heroku asset pipeline fix

--- config/application.rb
+++ config/application.rb
@@ -56,6 +56,9 @@ module TaggleCloud
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    # Heroku doesn't like database lookups on asset compilation
+    config.assets.initialize_on_precompile = false
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
