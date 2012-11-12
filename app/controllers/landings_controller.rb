ed95b7d14148343eb4d77cfdcac196a4e16a9381
Jonathan Bender <jlbender@gmail.com>
2012-11-12 15:48:49 -0500

    display usage stats on homepage

--- app/controllers/landings_controller.rb
+++ app/controllers/landings_controller.rb
@@ -1,5 +1,8 @@
 class LandingsController < ApplicationController
   def home
+    @attendee_count = Attendance.count
+    @connection_count = Connection.count
+    @conference_count = Conference.count
     render :layout => "landing"
   end
 end
