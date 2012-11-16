7b557c54f9838bcb608639554fca1b3b51519359
Nack Choon Jung <nackchoon@gmail.com>
2012-11-15 23:37:02 -0500

    stylesheet changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121111045820) do
+ActiveRecord::Schema.define(:version => 20121113191127) do
 
   create_table "abstract_tags", :force => true do |t|
     t.integer  "abstract_id"
@@ -66,6 +66,13 @@ ActiveRecord::Schema.define(:version => 20121111045820) do
     t.integer  "strength"
   end
 
+  create_table "coordinates", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "conference_id"
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
+  end
+
   create_table "emails", :force => true do |t|
     t.integer  "user_id"
     t.string   "mail_address"
@@ -100,6 +107,7 @@ ActiveRecord::Schema.define(:version => 20121111045820) do
     t.string   "occupation"
     t.string   "first_name"
     t.string   "last_name"
+    t.boolean  "is_admin"
   end
 
 end
