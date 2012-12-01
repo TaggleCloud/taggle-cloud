a871e3414b3168844b072e0f142bb83650a639fc
Jon <jetan2@gmail.com>
2012-12-01 15:16:55 -0500

    made like button pretty

--- db/schema.rb
+++ db/schema.rb
@@ -24,9 +24,9 @@ ActiveRecord::Schema.define(:version => 20121201035135) do
     t.integer  "user_id"
     t.integer  "attendance_id"
     t.text     "body"
-    t.datetime "created_at",                       :null => false
-    t.datetime "updated_at",                       :null => false
-    t.boolean  "is_bio",        :default => false
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
+    t.boolean  "is_bio"
   end
 
   create_table "attendances", :force => true do |t|
@@ -39,15 +39,6 @@ ActiveRecord::Schema.define(:version => 20121201035135) do
     t.string   "last_name"
     t.string   "organization"
     t.string   "project_name"
-  end
-
-  create_table "authentication", :force => true do |t|
-    t.integer  "authentication_id"
-    t.integer  "user_id"
-    t.string   "uid"
-    t.string   "provider"
-    t.datetime "created_at",        :null => false
-    t.datetime "updated_at",        :null => false
   end
 
   create_table "authentications", :force => true do |t|
