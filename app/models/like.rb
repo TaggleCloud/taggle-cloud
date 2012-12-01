95bf01aa4d5b5f9bf54b028fc3b88cccc7175d83
Jonathan Bender <jlbender@gmail.com>
2012-12-01 16:22:31 -0500

    liking works

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,8 +1,9 @@
 class Like < ActiveRecord::Base
   attr_accessible :attendance_id, :user_id
+  belongs_to :conference
   belongs_to :user
   belongs_to :attendance
   
-  
+  validates_uniqueness_of :user_id, :scope => :attendance_id
   
 end
