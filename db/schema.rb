f09ef3a9a021ee27ed99d2d724d307fbc98c0995
Jonathan Bender <jlbender@gmail.com>
2012-12-01 15:17:54 -0500

    removed table, donno why its not already checked in

--- db/schema.rb
+++ db/schema.rb
@@ -41,15 +41,6 @@ ActiveRecord::Schema.define(:version => 20121201035135) do
     t.string   "project_name"
   end
 
-  create_table "authentication", :force => true do |t|
-    t.integer  "authentication_id"
-    t.integer  "user_id"
-    t.string   "uid"
-    t.string   "provider"
-    t.datetime "created_at",        :null => false
-    t.datetime "updated_at",        :null => false
-  end
-
   create_table "authentications", :force => true do |t|
     t.integer  "authentication_id"
     t.integer  "user_id"
