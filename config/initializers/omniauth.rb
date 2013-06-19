62928d39f79d2d638210c85f51f0968a4b4ec4db
Alan Chang <alanchan@andrew.cmu.edu>
2013-06-19 16:50:28 -0400

    Google oauth2 changes

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -7,7 +7,8 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   else
     provider :developer
     provider :linkedin, "gaywpkqsa345", "3FpYnz57WmsJsTVA", :scope => 'r_fullprofile r_emailaddress r_network'
-    provider :google_oauth2, "179239928840.apps.googleusercontent.com", "lzcxOJO4N5LPcuTbo_pJ-Gbm", {:scope => 'userinfo.profile,userinfo.email,plus.me'}
+    provider :google_oauth2, "95071427023.apps.googleusercontent.com", "vkgDpDU8DBHE_lCEJHaIYxSL", {:scope => 'userinfo.profile,userinfo.email,plus.me'}    
+    # provider :google_oauth2, "179239928840.apps.googleusercontent.com", "lzcxOJO4N5LPcuTbo_pJ-Gbm", {:scope => 'userinfo.profile,userinfo.email,plus.me'}
     provider :facebook, "268051749965205", "71bb1eb11aac5b8c368a2f2e42a46bc0", {:image_size => 'large'}
   end
 end
