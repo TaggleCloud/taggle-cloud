65a500614bf519c0a542c122ff202008ffa81ee0
Alan Chang <alanchan@andrew.cmu.edu>
2013-08-02 13:04:22 -0400

    cosmetic changes

--- app/models/request.rb
+++ app/models/request.rb
@@ -3,6 +3,6 @@ class Request < ActiveRecord::Base
   belongs_to :user
   
   validates_format_of :email, :with => /^[\w]([^@\s,;]+)@(([\w-]+\.)+(com|edu|org|net|gov|mil|biz|info))$/i, :message => "is not a valid format", :allow_blank => true
-  validates_format_of :phone, :with => /\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/, :message => "is not a valid format", :allow_blank => true
+  validates_format_of :phone, :with => /^\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})$/, :message => "is not a valid format", :allow_blank => true
   validates_uniqueness_of :user_id, :scope => :inviter
 end
